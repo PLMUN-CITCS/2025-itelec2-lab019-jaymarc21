ff22ed2186e17430c4aedb06ccdb504d718052ad
jaymarc21 <>
2025-03-08 12:59:04 +0800

    Last Sync: 2025-03-08 04:59:04 (Mobile)

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -1,3 +1,5 @@
-get_student_score().
-calculate _grade(score).
-print().
+student_score = get_student_score()  # Get student score
+grade = calculate_grade(student_score)  # Determine grade
+
+# Display the result
+print(f"Your Grade is: {grade}")
